247a60922c0b0303b873bf4e1490251c4ee9f6ef
Beri28 <88897721+Beri28@users.noreply.github.com>
2023-03-26 12:50:24 +0100

    Update dayOne.js
    Adding some comments

--- dayOne.js
+++ dayOne.js
@@ -1,3 +1,7 @@
+// function to find the sum of array elements
+// Function takes in an array of integers as parameter
+// And returns sum of its elements as an integer
+
 function arraySum(ar){
     let sum=0;
     for(let i=0;i<ar.length;i++){
