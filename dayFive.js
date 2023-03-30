abae1b62ee9ebc8fa4ce4ff65b3e7206a3ed0aeb
Beri28 <88897721+Beri28@users.noreply.github.com>
2023-03-30 22:23:56 +0100

    Day five solution

---
+++ dayFive.js
@@ -0,0 +1,27 @@
+const valleys=(steps,path)=>{
+	path=path.toUpperCase()
+ 	let height=0;
+ 	let u=1;
+ 	let d=-1;
+ 	let valleys=0;
+ 	let seaLevel=0;
+ 	let currentStep=0;
+ 	for(let i=0;i<path.length;i++){
+ 		 if(path[i]=='D'){
+ 			if(height==seaLevel){
+ 				valleys++;
+ 			}
+ 			height=height+d
+ 		}
+ 		else if(path[i]=='U'){
+ 			height=height+u
+ 		}
+
+ 		
+ 		
+ 	}
+ 	console.log(valleys)
+}
+valleys(8,"UDDDUDUU")
+valleys(3,"DDUUUUDD")
+valleys(12,"DDUUDDUUUUDD")
