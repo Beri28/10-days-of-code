80d97535d8c9ff810b1cbe15df2e36708f603ead
Beri28 <88897721+Beri28@users.noreply.github.com>
2023-04-01 19:22:22 +0100

    Day seven's solution

---
+++ daySeven.js
@@ -0,0 +1,23 @@
+function birthCakeCandles(Candles){
+    let counter=0;
+    let biggest=Candles[0];
+    for(let i=1;i<+Candles.length;i++){
+        if(biggest>Candles[i]){
+            continue;
+        }
+        else if (biggest<Candles[i]){
+            biggest=Candles[i];
+        }
+    }
+    for(let i=0;i<Candles.length;i++){
+        if(Candles[i]==biggest){
+            counter++;
+        }
+    }
+    console.log(biggest);
+    console.log(counter)
+    return counter;
+    
+}
+birthCakeCandles([3,2,1,5])
+birthCakeCandles([1,2,3,10,21,4,6,7,21,0])
