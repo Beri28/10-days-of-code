8a55a0de6d3479e31e357d0f5338bd328c9b6c51
Beri28 <88897721+Beri28@users.noreply.github.com>
2023-04-03 13:58:05 +0100

    Day nine's solution

---
+++ dayNine.js
@@ -0,0 +1,17 @@
+function catAndMouse(x,y,z){
+    let diff1=z-x;
+    diff1=Math.abs(diff1);
+    let diff2=z-y;
+    diff2=Math.abs(diff2);
+    if(diff1>diff2){
+        return 'Cat B'
+    }
+    else if(diff1<diff2){
+        return 'Cat A'
+    }
+    else{
+        return 'MOuse C'
+    }
+}
+console.log(catAndMouse(1,2,3))
+console.log(catAndMouse(1,3,2))
