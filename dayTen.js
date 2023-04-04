a65e591f734a60cd597b888df41f64a9ebda3d60
Beri28 <88897721+Beri28@users.noreply.github.com>
2023-04-04 13:53:49 +0100

    Day ten's solution

---
+++ dayTen.js
@@ -0,0 +1,19 @@
+function kangaroo(x1,v1,x2,v2){
+    let kd1=x1;
+    let kd2=x2;
+    let decision='NO';
+    for(let i=0;i<10000;i++){
+        kd1=kd1+v1;
+        kd2=kd2+v2;
+        if(kd1==kd2){
+            decision='YES';
+            break;
+        }
+    }
+    console.log(decision)
+    return decision;
+}
+kangaroo(0,3,4,2)
+kangaroo(2,1,1,2)
+kangaroo(0,2,5,3)
+
